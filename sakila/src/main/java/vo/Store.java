0170c2308ea9d88adc3aece2b901523162cdb08e
goodee <goodee@DESKTOP-RLH7GB3>
2022-04-01 16:41:08 +0900

    storeVo getter, setter 추가

--- sakila/src/main/java/vo/Store.java
+++ sakila/src/main/java/vo/Store.java
@@ -5,4 +5,28 @@ public class Store {
 	private int managerStaffId;
 	private int addressId;
 	private String lastUpdate;
+	public int getStoreId() {
+		return storeId;
+	}
+	public void setStoreId(int storeId) {
+		this.storeId = storeId;
+	}
+	public int getManagerStaffId() {
+		return managerStaffId;
+	}
+	public void setManagerStaffId(int managerStaffId) {
+		this.managerStaffId = managerStaffId;
+	}
+	public int getAddressId() {
+		return addressId;
+	}
+	public void setAddressId(int addressId) {
+		this.addressId = addressId;
+	}
+	public String getLastUpdate() {
+		return lastUpdate;
+	}
+	public void setLastUpdate(String lastUpdate) {
+		this.lastUpdate = lastUpdate;
+	}
 }
